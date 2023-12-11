a6e9ff4df78a0db2eaf17d35cff3897a23506a45
KrieqqeR <burakcevizli@gmail.com>
2023-12-11 22:37:16 +0300

    computed learned v for , class @click learned

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
-            showBooks:true,
-            title:"The Final Empire 2",
-            author:"Brandon Sanderson",
-            age:45
+            url: 'www.google.com',
+            showBooks: true,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'https://www.semakirtasiye.com.tr/_contents/170969_e14603777043.png' ,isFav:true},
+                { title: 'theway of the kings', author: 'branddd', img: 'https://www.semakirtasiye.com.tr/_contents/170969_e14603777043.png' ,isFav:false},
+                { title: 'CEvizle', author: 'Burak', img: 'https://www.semakirtasiye.com.tr/_contents/170969_e14603777043.png' ,isFav:true},
+            ]
         }
     },
-    methods:{
-        toggleShowBook(){
-        this.showBooks= !this.showBooks
-       }
+    methods: {
+        toggleFav(book){
+            book.isFav = !book.isFav
+        },
+        toggleShowBook() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
+    },
+    computed:{
+        filteredBooks(){
+            return this.books.filter((book)=>book.isFav)
+        }
     }
 })
 
