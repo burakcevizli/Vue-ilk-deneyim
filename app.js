9ddb2a52fa222c3234a4c81cee1cdc30a6a87733
KrieqqeR <burakcevizli@gmail.com>
2023-12-11 21:33:42 +0300

    click dom , learned

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const app = Vue.createApp({
     data(){
         return {
+            showBooks:true,
             title:"The Final Empire 2",
             author:"Brandon Sanderson",
             age:45
         }
+    },
+    methods:{
+        toggleShowBook(){
+        this.showBooks= !this.showBooks
+       }
     }
 })
 
